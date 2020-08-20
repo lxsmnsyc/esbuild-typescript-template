a2c94c84c177d854508922aa8954c424a3a80866
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-08-20 13:09:27 +0800

    Fix build-src to async block

--- build-src.js
+++ build-src.js
@@ -3,6 +3,8 @@ const fs = require('fs-extra');
 const config = require('./esbuild.config');
 const pkg = require('./package.json');
 
+console.time('ESBuild');
+
 function getPackageName(name) {
   return name
     .toLowerCase()
@@ -13,99 +15,97 @@ const baseDir = `./dist`;
 const baseName = getPackageName(pkg.name);
 const baseOut = `${baseDir}/${baseName}`;
 
-// DEV build
-console.time('Development Build');
-build({
-  entryPoints: [
-    config.entry,
-  ],
-  outfile: `${baseOut}.development.js`,
-  bundle: true,
-  // Never minify dev builds
-  minify: false,
-  // Generate source maps for dev builds
-  sourcemap: true,
-  // define environment variables
-  define: config.define,
-  // 
-  target: config.target,
-  // 
-  platform: config.platform,
-  //
-  tsconfig: config.tsconfig,
-}).then(
-  () => {
-    console.timeEnd('Development Build');
-  },
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  },
-);
+async function buildAll() {
+  await fs.remove('./dist');
 
-// Prod build
-console.time('Production Build');
-build({
-  entryPoints: [
-    config.entry,
-  ],
-  outfile: `${baseOut}.production.min.js`,
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  // define environment variables
-  define: config.define,
-  // 
-  target: config.target,
-  // 
-  platform: config.platform,
-  //
-  tsconfig: config.tsconfig,
-}).then(
-  () => {
-    console.timeEnd('Production Build');
-  },
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  },
-);
+  console.time('Development Build');
+  console.time('Production Build');
+  console.time('ESM Build');
+  await Promise.all([
+    // DEV build
+    build({
+      entryPoints: [
+        config.entry,
+      ],
+      outfile: `${baseOut}.development.js`,
+      bundle: true,
+      // Never minify dev builds
+      minify: false,
+      // Generate source maps for dev builds
+      sourcemap: true,
+      // define environment variables
+      define: config.define,
+      // 
+      target: config.target,
+      // 
+      platform: config.platform,
+      //
+      tsconfig: config.tsconfig,
+    }).then(
+      () => console.timeEnd('Development Build'),
+    ),
+    // PROD build
+    build({
+      entryPoints: [
+        config.entry,
+      ],
+      outfile: `${baseOut}.production.min.js`,
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      // define environment variables
+      define: config.define,
+      // 
+      target: config.target,
+      // 
+      platform: config.platform,
+      //
+      tsconfig: config.tsconfig,
+    }).then(
+      () => {
+        console.timeEnd('Production Build');
+      },
+    ),
+    // ESM build
+    build({
+      entryPoints: [
+        config.entry,
+      ],
+      outfile: `${baseOut}.esm.js`,
+      bundle: true,
+      minify: false,
+      format: 'esm',
+      sourcemap: false,
+      // define environment variables
+      define: config.define,
+      // 
+      target: config.target,
+      //
+      tsconfig: config.tsconfig,
+    }).then(
+      () => {
+        console.timeEnd('ESM Build');
+      },
+    ),
+  ]);
 
-// ESM build
-console.time('ESM Build');
-build({
-  entryPoints: [
-    config.entry,
-  ],
-  outfile: `${baseOut}.esm.js`,
-  bundle: true,
-  minify: false,
-  format: 'esm',
-  sourcemap: false,
-  // define environment variables
-  define: config.define,
-  // 
-  target: config.target,
-  //
-  tsconfig: config.tsconfig,
-}).then(
-  () => {
-    console.timeEnd('ESM Build');
-  },
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  },
-);
+  const baseLine = `module.exports = require('./${baseName}`;
+  const contents = `
+  'use strict'
+  if (process.env.NODE_ENV === 'production') {
+    ${baseLine}.production.min.js')
+  } else {
+    ${baseLine}.development.js')
+  }
+  `;
 
-const baseLine = `module.exports = require('./${baseName}`;
-const contents = `
-'use strict'
-if (process.env.NODE_ENV === 'production') {
-  ${baseLine}.production.min.js')
-} else {
-  ${baseLine}.development.js')
+  await fs.outputFile('./dist/index.js', contents);
+  console.timeEnd('ESBuild');
 }
-`;
 
-fs.outputFile('./dist/index.js', contents);
+buildAll().catch(
+  (err) => {
+    console.error(err);
+    process.exit(1);
+  },
+);
